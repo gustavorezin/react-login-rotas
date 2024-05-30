de41dd64967f2bcf312e7abf4e5c0a2c88c98e2c
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-05-30 20:24:59 -0300

    :bug: fix: redirecionamento login

--- src/pages/_layouts/app/index.tsx
+++ src/pages/_layouts/app/index.tsx
@@ -1,20 +1,33 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useContext } from "react";
 import { AuthContext } from "@/contexts/auth-contexts";
-import { Container, SignOut } from "./styles";
+import { useContext } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import {
+  Container,
+  ContainerNotAuthorizated,
+  LinkGoBack,
+  SignOut,
+} from "./styles";
 
 export function AppLayout() {
   const { isAuthenticated, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  if (!isAuthenticated) {
-    return <Navigate to={"/auth/sign-in"} />;
+  function handleLoginRedirect() {
+    navigate("/auth/sign-in", { replace: true });
   }
 
   return (
     <Container>
       <span>ROTA PRIVADA</span>
       <SignOut onClick={logout}>Sair</SignOut>
-      <Outlet />
+      {isAuthenticated ? (
+        <Outlet />
+      ) : (
+        <ContainerNotAuthorizated>
+          <h1>Sessão expirada</h1>
+          <LinkGoBack onClick={handleLoginRedirect}>Fazer login</LinkGoBack>
+        </ContainerNotAuthorizated>
+      )}
     </Container>
   );
 }
--- src/pages/_layouts/app/styles.ts
+++ src/pages/_layouts/app/styles.ts
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+const breakpoints = {
+  small: "576px",
+  medium: "768px",
+  large: "992px",
+  xlarge: "1200px",
+};
+
 export const Container = styled.div`
   position: relative;
 
@@ -18,4 +25,35 @@ export const SignOut = styled.a`
   right: 6px;
   color: ${(props) => props.theme.colors.red};
   cursor: pointer;
+`;
+
+export const ContainerNotAuthorizated = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  padding: 0 1rem;
+
+  h1 {
+    text-align: center;
+    font-size: 2rem;
+
+    @media (min-width: ${breakpoints.medium}) {
+      font-size: 4rem;
+    }
+  }
+`;
+
+export const LinkGoBack = styled.a`
+  text-decoration: none;
+  color: ${(props) => props.theme.colors.red};
+  transition: 0.3s ease;
+
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.9;
+  }
 `;
